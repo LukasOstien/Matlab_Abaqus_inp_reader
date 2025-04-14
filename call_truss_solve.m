197e9dd7c2dcecc41c56befc007b50fa942f9d58
Lukas Ostien <lukas.ostien@gmail.com>
2025-04-14 10:46:56 -0700

    Update call_truss_solve.m

--- call_truss_solve.m
+++ call_truss_solve.m
@@ -3,5 +3,6 @@ clear all %#ok<CLALL>
 clc
 close all
 
-% Call inp file by sending a text file to the truss_solve function
+% Process and solve inp file by sending an Abaqus .inp file to the truss_solve function
+% As mentioned in disclaimer, 2D truss system only
 truss_solve('[your Abaqus .inp file].txt');
